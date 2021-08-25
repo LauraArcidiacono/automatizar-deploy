d048cbefc91b32dfc62b179dcd2f130a3594142a
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-08-25 14:36:26 +0200

    Configure work space

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Laura Deploy GitHub
+        </p>
+        <p>
+          No por mucho madrugar se amanece más temprano
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Merci Lluc
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
